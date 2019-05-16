219b3c47128ef2d3c075178f8ccf346ea9bc1181
Атанас Станчев <mr_Stanchev@hotmail.com>
2019-05-16 22:33:07 +0300

    Commit

---
+++ 08. Balanced Parenthesis/BalancedParenthesis.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Balanced_Parenthesis
+{
+    class BalancedParenthesis
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> parentheses = new Stack<char>();
+            char[] openParenthese = new[] { '{', '[', '(' };
+
+            string input = Console.ReadLine();
+            bool isValid = true;
+
+            foreach (var ch in input)
+            {
+                if (openParenthese.Contains(ch))
+                {
+                    parentheses.Push(ch);
+                    continue;
+                }
+
+                if (parentheses.Count == 0)
+                {
+                    isValid = false;
+                    break;
+                }
+
+                if (ch == ')')
+                {
+                    if (parentheses.Peek() == '(')
+                    {
+                        parentheses.Pop();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else if (ch == ']')
+                {
+                    if (parentheses.Peek() == '[')
+                    {
+                        parentheses.Pop();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else if (ch == '}')
+                {
+                    if (parentheses.Peek() == '{')
+                    {
+                        parentheses.Pop();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (parentheses.Count == 0 && isValid)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+
+        }
+    }
+}

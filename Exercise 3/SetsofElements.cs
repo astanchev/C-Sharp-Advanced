1d3ee7f5e48a40d86ddbc9f919f537918a9b4213
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-05-25 08:33:09 +0300

    Add files via upload

---
+++ Exercise 3/SetsofElements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Sets_of_Elements
+{
+    class SetsofElements
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> namesN = new HashSet<string>();
+            HashSet<string> namesM = new HashSet<string>();
+            HashSet<string> repeated = new HashSet<string>();
+            string[] nAndm = Console.ReadLine().Split();
+            int n = int.Parse(nAndm[0]);
+            int m = int.Parse(nAndm[1]);
+
+            for (int i = 0; i < n + m; i++)
+            {
+                if (i < n)
+                {
+                    namesN.Add(Console.ReadLine());
+                }
+                else
+                {
+                    namesM.Add(Console.ReadLine());
+                }
+            }
+
+            foreach (var name in namesN)
+            {
+                if (namesM.Contains(name))
+                {
+                    repeated.Add(name);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", repeated));
+        }
+    }
+}

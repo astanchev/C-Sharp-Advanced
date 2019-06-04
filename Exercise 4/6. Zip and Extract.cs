a9916e201737acf7aed475224dea76eb72481188
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-04 14:58:07 +0300

    Add files via upload

---
+++ Exercise 4/6. Zip and Extract.cs
@@ -0,0 +1,21 @@
+﻿namespace _6._Zip_and_Extract
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "copyMe.png";
+            string pathDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string outputFileNAme = @"myZip.zip";
+            string outputPath = Path.Combine(pathDesktop, outputFileNAme);
+            
+            using (ZipArchive archive = ZipFile.Open(outputPath, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(filePath, Path.GetFileName(filePath));
+            }
+        }
+    }
+}

48f841d74d886ded641dcf1acbbc6f4363751659
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-04 14:59:23 +0300

    Add files via upload

---
+++ Lesson 4/2. Line Numbers.cs
@@ -0,0 +1,31 @@
+﻿namespace _2._Line_Numbers
+{
+    using System;
+    using System.IO;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"files/02. Line Numbers/";
+            string fileInput = @"Input.txt";
+            string fileOutput = @"Output.txt";
+            string inputPath = Path.Combine(path, fileInput);
+            string outputPath = Path.Combine(path, fileOutput);
+
+            using (StreamReader reader = new StreamReader(inputPath))
+            {
+                int counter = 1;
+                string line = reader.ReadLine();
+
+                using (StreamWriter writer = new StreamWriter(outputPath))
+                {
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter++}. {line}");
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}

8e40bcbe914e5f1001610a64ad0ec486c49fafa3
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-04 14:56:28 +0300

    Add files via upload

---
+++ Exercise 4/2. Line Numbers.cs
@@ -0,0 +1,27 @@
+﻿namespace _2._Line_Numbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"text.txt";
+            string outputPath = @"output.txt";
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            int count = 1;
+
+            foreach (var line in lines)
+            {
+                int lettersCount = line.Count(char.IsLetter);
+                int puncsCount = line.Count(char.IsPunctuation);
+
+                File.AppendAllText(outputPath,$"Line {count}: {line} ({lettersCount})({puncsCount}){Environment.NewLine}");
+                count++;
+            }
+        }
+    }
+}

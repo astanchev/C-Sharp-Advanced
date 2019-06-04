303a785b8e133991721fff3376488b29c5af0d18
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-04 15:01:03 +0300

    Add files via upload

---
+++ Lesson 4/6. Folder Size.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace _6._Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"..\..\..\files\06. Folder Size\TestFolder\";
+            string[] files = Directory.GetFiles(path);
+            double sum = 0;
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                sum += fileInfo.Length;
+            }
+            sum = sum / 1024 / 1024;
+            File.WriteAllText("оutput.txt", sum.ToString());
+
+        }
+    }
+}

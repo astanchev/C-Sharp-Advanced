c5cd5796d463b1547eea7c1671d68f0a241fabe4
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-04 14:57:22 +0300

    Add files via upload

---
+++ Exercise 4/4. Copy Binary File.cs
@@ -0,0 +1,34 @@
+﻿namespace _4._Copy_Binary_File
+{
+    using System;
+    using System.IO;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = "copyMe.png";
+            string outputPath = "copyMe_Copied.png";
+
+            using (FileStream inputFile = new FileStream(inputPath, FileMode.Open))
+            {
+                using (FileStream outputFile = new FileStream(outputPath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[4096];
+
+                    while (true)
+                    {
+                        int size = inputFile.Read(buffer, 0, buffer.Length);
+
+                        if (size == 0)
+                        {
+                            break;
+                        }
+
+                        outputFile.Write(buffer, 0, size);
+
+                    }
+                }
+            }
+        }
+    }
+}

5a6e3f2dab77162e6e623b6d5026110e2cb8a9bd
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-06 00:25:35 +0300

    Add files via upload

---
+++ Lesson 5/CountUppercaseWords.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._2_Count_Uppercase_Words
+{
+    class CountUppercaseWords
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => char.IsUpper(w[0]))
+                .Select(w =>
+                {
+                    Console.WriteLine(w);
+                    return w;
+                })
+                .ToList();
+        }
+    }
+}

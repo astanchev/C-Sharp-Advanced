5ece9d9870158d4f18e270f73b56ea6c52b83ea9
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-06 00:24:38 +0300

    Add files via upload

---
+++ Lesson 5/02. Sum Numbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    class SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(Parse)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+
+        public static Func<string, int> Parse = x => int.Parse(x);
+
+    }
+}

020499664e9e868940279e01523221fdbc6c67a4
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-05 22:29:05 +0300

    Add files via upload

---
+++ Lesson 6/Problem 2/Car.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity 
+        {
+            get
+            {
+                return this.fuelQuantity;
+            }
+            set
+            {
+                this.fuelQuantity = value;
+            }
+        }
+
+        public double FuelConsumption 
+        {
+            get
+            {
+                return this.fuelConsumption;
+            }
+            set
+            {
+                this.fuelConsumption = value;
+            }
+        }
+
+        public void Drive(double distance)
+        {
+            bool canContinue = this.FuelQuantity - (distance * this.FuelConsumption/100) >= 0;
+            if (canContinue)
+            {
+                this.FuelQuantity -= distance * this.FuelConsumption/100;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"Year: {this.Year}");
+            sb.Append($"Fuel: {this.FuelQuantity:f2}L");
+            return sb.ToString();
+        }
+    }
+}

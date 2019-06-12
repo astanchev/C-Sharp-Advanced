68f79c31ea67ad715a686b6b5094c06f7c788d9e
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-12 17:49:08 +0300

    Add files via upload

---
+++ Exercise 7/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WorkShop
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var doublyLinkedList = new DoublyLinkedList();
+
+            doublyLinkedList.AddFirst(1);
+            doublyLinkedList.AddFirst(2);
+            doublyLinkedList.AddFirst(3);
+
+            doublyLinkedList.Print(Console.WriteLine);
+        }
+    }
+}

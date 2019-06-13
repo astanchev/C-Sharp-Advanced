14965796bc32bec7c71a3155c8ef6cf6931bb86b
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-14 00:31:11 +0300

    Add files via upload

---
+++ Exercise 8/Problem 1/Box.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace _01.GenericBoxofString
+{
+    public class Box<TItem>
+    {
+        private List<TItem> boxItems;
+
+        public Box()
+        {
+            boxItems = new List<TItem>();
+        }
+
+        public void Add(TItem item)
+        {
+            boxItems.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var boxItem in boxItems)
+            {
+                sb.AppendLine($"{boxItem.GetType().FullName}: {boxItem}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+        
+    }
+}
---
+++ Exercise 8/Problem 1/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01.GenericBoxofString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            int numberLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberLines; i++)
+            {
+                box.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(box);
+        }
+    }
+}

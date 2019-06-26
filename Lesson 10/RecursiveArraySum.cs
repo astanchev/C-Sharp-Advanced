abb4602a23d66bd2606243f6487346aa51aa7b57
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-27 00:03:12 +0300

    Add files via upload

---
+++ Lesson 10/RecursiveArraySum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Recursive_Array_Sum
+{
+    class RecursiveArraySum
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(Sum(arr, 0));
+        }
+
+        
+        private static int Sum(int[] arr, int index)
+        {
+            if (index == arr.Length -1)
+            {
+                return arr[arr.Length - 1];
+            }
+
+            return arr[index] + Sum(arr, index + 1);
+        }
+    }
+}

bed8fafa0833d1243d941d27d45c2a8cae20dbf3
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-27 00:03:55 +0300

    Add files via upload

---
+++ Lesson 10/RecursiveFactorial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Recursive_Factorial
+{
+    class RecursiveFactorial
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Factorial(number));
+        }
+
+        private static long Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return number * Factorial(--number);
+        }
+    }
+}
